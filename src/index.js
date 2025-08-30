2a6a72b125a26fe7506f7fbfb159f9c0097d7cb8
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-30 08:27:27 +0100

    updated the html.
    debugging system. it is not working

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ let LosAngelesCurrent = moment().tz("America/Los_Angeles");
 
 
 losAngelesDateElement.innerHTML = losAngelesCurrent.moment().format("Do MMMM YYYY");
-losAngelesTimeElement.innerHTML = losAngelesCurrent.LosAngelesCurrentmoment().format("h:mn:ss [<small>]A[</samall>]");
+losAngelesTimeElement.innerHTML = losAngelesCurrent.moment().format("h:mn:ss [<small>]A[</samall>]");
