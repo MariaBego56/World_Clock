848b8fa7df2eada73d714370a4fa91806558324c
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-30 08:18:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement(".date");
 let losAngelesTimeElement = losAngelesElement(".time");
-let LosAngelesTime = moment().tz("America/Los_Angeles");
+let LosAngelesCurrent = moment().tz("America/Los_Angeles");
 
 
-losAngelesDateElement.innerHTML = moment().format("Do MMMM YYYY");
-losAngelesTimeElement.innerHTML = moment().format("h:mn:ss [<small>]A[</samall>]");
+losAngelesDateElement.innerHTML = losAngelesCurrent.moment().format("Do MMMM YYYY");
+losAngelesTimeElement.innerHTML = losAngelesCurrent.LosAngelesCurrentmoment().format("h:mn:ss [<small>]A[</samall>]");
