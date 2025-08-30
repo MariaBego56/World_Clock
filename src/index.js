ef83bdd71a9a2f3a4c7e0583a8ae77ba5430af5c
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-30 08:32:54 +0100

    reinstate the link to the css file

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ let LosAngelesCurrent = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesCurrent.moment().format("Do MMMM YYYY");
 losAngelesTimeElement.innerHTML = losAngelesCurrent.moment().format("h:mn:ss [<small>]A[</samall>]");
+
+
+
+let ParisElement = document.querySelector("#paris");
+let ParisDateElement = ParisElement(".date");
+let ParisTimeElement = ParisElement(".time");
+let ParisCurrent = moment().tz("Europe/Paris");
+
+
+ParisDateElement.innerHTML = ParisCurrent.moment().format("Do MMMM YYYY");
+ParisTimeElement.innerHTML = ParisCurrent.moment().format("h:mn:ss [<small>]A[</samall>]");
