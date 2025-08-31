acd6a330839e58010506ef316fd0bfe5b504a0ff
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-31 22:52:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function updateTime() {
 
 let losAngeles = document.querySelector("#los-angeles");
-if (losAngeles){
+
 
 let losAngelesDate = losAngeles.querySelector(".date");
 let losAngelesTime = losAngeles.querySelector(".time");
@@ -9,10 +9,10 @@ let losAngelesCurrent = moment().tz("America/Los_Angeles");
 
 losAngelesDate.innerHTML = losAngelesCurrent.format("Do MMMM YYYY");
 losAngelesTime.innerHTML = `${losAngelesCurrent.format("h:mm:ss")}<small> ${losAngelesCurrent.format("A") }</small>`;
-}
+
 
 let paris = document.querySelector("#paris");
-if (paris){
+
 let parisDate = paris.querySelector(".date");
 let parisTime = paris.querySelector(".time");
 let parisCurrent = moment().tz("Europe/Paris");
@@ -21,13 +21,13 @@ parisDate.innerHTML = parisCurrent.format("Do MMMM YYYY");
 parisTime.innerHTML = `${parisCurrent.format("h:mm:ss")}<small> ${parisCurrent.format("A") }</small>`;
 
 }
-}
 
 function updateCity(event) {
 /* event.target.value */
 if (cityTimeZone === "current"){
   cityTimeZone = moment.tz.guess();
 }
+
 let cityTimeZone = event.target.value;
 let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
