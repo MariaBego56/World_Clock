cd4354fc8bb93c0554dda10f2aa718a1c1bf0191
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-31 21:35:16 +0100

    make select functional
    create an id select.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 function updateTime() {
 
 let losAngeles = document.querySelector("#los-angeles");
+if (losAngeles){
+
 let losAngelesDate = losAngeles.querySelector(".date");
 let losAngelesTime = losAngeles.querySelector(".time");
-
 let losAngelesCurrent = moment().tz("America/Los_Angeles");
+
 losAngelesDate.innerHTML = losAngelesCurrent.format("Do MMMM YYYY");
 losAngelesTime.innerHTML = losAngelesCurrent.format("h:mm:ss") + "<small>" + losAngelesCurrent.format("A") + "</small>";
+}
 
 let paris = document.querySelector("#paris");
+if (paris){
 let parisDate = paris.querySelector(".date");
 let parisTime = paris.querySelector(".time");
 
@@ -18,5 +22,30 @@ parisTime.innerHTML = parisCurrent.format("h:mm:ss") + "<small>" + parisCurrent.
 
 }
 
+function updateCity(event) {
+/* event.target.value */
+let cityTimeZone = event.target.value;
+let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+let cityTime = moment().tz(cityTimeZone);
+let cities = document.querySelector("#cities");
+cities.innerHTML = `
+<div class="city-time">
+        <div class="city-date">
+          <div class="city">
+            <h2>${cityName}</h2>
+          </div>
+          <div class="date">${cityTime.format("Do MMMM YYYY")} </div>
+        </div>
+        <div class="time">${cityTime.format("h:mn:ss")}+ "<small> ${cityTime.format("A")} + "</small>"
+        </div>
+      </div> `;
+
+}
+
+
 updateTime ();
 setInterval (updateTime, 1000);
+
+let citiesSelect = document.querySelector("#city-dropdown");
+
+citiesSelect. addEventListener("change", updateCity);
