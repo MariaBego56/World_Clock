7b21adcd2d6e7ae4c2bad58a6a198f25ecc48cc1
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-31 13:20:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ let losAngeles = document.querySelector("#los-angeles");
 let losAngelesDate = losAngeles.querySelector(".date");
 let losAngelesTime = losAngeles.querySelector(".time");
 
+let losAngelesCurrent = moment().tz("America/Los_Angeles");
+losAngelesDate.innerHTML = losAngelesCurrent.format("Do MMMM YYYY");
+losAngelesTime.innerHTML = losAngelesCurrent.format("h:mm:ss") + "<small>" + losAngelesCurrent.format("A") + "</small>";
 
 let paris = document.querySelector("#paris");
 let parisDate = paris.querySelector(".date");
 let parisTime = paris.querySelector(".time");
 
-function updateTimeDate(){
-
-let losAngelesCurrent = moment().tz("America/Los_Angeles");
-losAngelesDate.innerHTML = losAngelesCurrent.format("Do MMMM YYYY");
-losAngelesTime.innerHTML = losAngelesCurrent.format("h:mm:ss") + "<small>" + losAngelesCurrent.format("A") + "</small>";
 
 let parisCurrent = moment().tz("Europe/Paris");
 parisDate.innerHTML = parisCurrent.format("Do MMMM YYYY");
 parisTime.innerHTML = parisCurrent.format("h:mm:ss [<small>]A[</small>]");
-}
