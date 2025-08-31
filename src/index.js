211824e4855290e93f48e9d0656dda99334c3d4b
MariaBego56 <148390279+MariaBego56@users.noreply.github.com>
2025-08-31 11:15:21 +0100

    updated the js file
    the date and time still not working. The layout seems to work now..

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ let losAngelesDate = losAngeles.querySelector(".date");
 let losAngelesTime = losAngeles.querySelector(".time");
 
 
-let ParisElement = document.querySelector("#paris");
-let ParisDateElement = ParisElement.querySelector(".date");
-let ParisTimeElement = ParisElement.querySelector(".time");
+let paris = document.querySelector("#paris");
+let parisDate = paris.querySelector(".date");
+let parisTime = paris.querySelector(".time");
 
 function updateTimeDate(){
 
@@ -13,10 +13,7 @@ let losAngelesCurrent = moment().tz("America/Los_Angeles");
 losAngelesDate.innerHTML = losAngelesCurrent.format("Do MMMM YYYY");
 losAngelesTime.innerHTML = losAngelesCurrent.format("h:mm:ss") + "<small>" + losAngelesCurrent.format("A") + "</small>";
 
-let ParisCurrent = moment().tz("Europe/Paris");
-ParisDateElement.innerHTML = ParisCurrent.format("Do MMMM YYYY");
-ParisTimeElement.innerHTML = ParisCurrent.format("h:mm:ss [<small>]A[</small>]");
-
-
+let parisCurrent = moment().tz("Europe/Paris");
+parisDate.innerHTML = parisCurrent.format("Do MMMM YYYY");
+parisTime.innerHTML = parisCurrent.format("h:mm:ss [<small>]A[</small>]");
 }
-
